7d7d7dc371251caddac86fde1a776cfc416e1c51
Kateryna <katemart17@gmail.com>
2023-10-09 17:01:07 -0400

    Kateryna_Reverse_Negative_Number

---
+++ src/week3/Kateryna_Reverse_Negative_Number.java
@@ -0,0 +1,32 @@
+package week3;
+
+public class Kateryna_Reverse_Negative_Number {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse_Negative_N(-56784321));
+
+    }
+
+    public static int reverse_Negative_N(int number) {
+
+        if (number >= 0) {
+            System.out.println("Number is positive, enter negative number");
+        }
+
+        String answerS = "-";
+        String n = Integer.toString(number);
+
+        for (int i = n.length()-1; i >= 1 ; i--) {
+
+            answerS += n.charAt(i);
+
+        }
+
+        int answerInt = Integer.parseInt(answerS);
+
+        return answerInt;
+
+    }
+
+}
