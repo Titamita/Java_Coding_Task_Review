21ca615956ed351d501b8b91c9345c2da034c2c7
DSC9000 <devolution.sdc@gmail.com>
2023-10-10 14:29:04 -0500

    renamed the methods with name

--- src/week4/dan_FrequencyOfCaracters.java
+++ src/week4/dan_FrequencyOfCaracters.java
@@ -1,21 +1,35 @@
 package src.week4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FrequencyOfCaracters {
     public static void main(String[] args) {
-        /*
+    /*
         return method find frequency of chars in a string
          */
-        String str= "aaabbbbccc";
-        System.out.println(removeDup("aaabbbccc"));
-    }
-    public static String removeDup(String str){
-       String out="";
+        String str= "aaabbbbcccqqqqqqq";
 
-       for(int i =0;i<str.length();i++){
-           if(!out.contains(str.substring(i,i+1))){
-               out+=str.substring(i,i+1);
-           }
-       }
-       return out;
+        Map<Character, Integer> result = CharFrequency(str);
+
+        // Print the character frequencies
+        for (Map.Entry<Character, Integer> entry : result.entrySet()) {
+            System.out.print( " "+ entry.getKey()+  entry.getValue());
+        }
+    }
+    public static Map<Character, Integer> CharFrequency(String input) {
+
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+
+        for (char ch : input.toCharArray()) {
+
+            if (frequencyMap.containsKey(ch)) {
+                frequencyMap.put(ch, frequencyMap.get(ch) + 1);
+            } else {
+                frequencyMap.put(ch, 1);
+            }
+        }
+        return frequencyMap;
     }
 }
