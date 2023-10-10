339935ab25b172e896add044478ff252d18feab6
DSC9000 <devolution.sdc@gmail.com>
2023-10-10 08:58:01 -0500

    dan prime and reverse

--- src/week3/dan_reverseNegativeNumber.java
+++ src/week3/dan_reverseNegativeNumber.java
@@ -6,12 +6,15 @@ public class dan_reverseNegativeNumber {
 
     public static void main(String[] args) {
         int num=-12345;
+        System.out.println(reverse(num));
+    }
+    public static int reverse(int num){
         int reverse=0;
         while(num!=0){
-            int last = num%10;
+            int last = num % 10;
             reverse=reverse*10+last;
             num /=10;
         }
-        System.out.println(reverse);
+        return num;
     }
 }
