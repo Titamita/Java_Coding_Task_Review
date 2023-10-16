e2f22a144d145e5ec209f3dfbf54e8f16424d0e0
ElenaKukoska0910 <ekukoska@gmail.com>
2023-10-16 17:11:53 -0400

    Function Reverse Negative Number

---
+++ src/week3/ReverseNegativeNumberElena.java
@@ -0,0 +1,19 @@
+package src.week3;
+
+public class ReverseNegativeNumberElena {
+    public static int reverseNegativeNumber(int num){
+        if(num >= 0){
+            return num;
+        }else{
+            return -num;
+        }
+    }
+
+    public static void main(String[] args) {
+        int negativeNumber = -12345;
+        int reversed = reverseNegativeNumber(negativeNumber);
+
+        System.out.println(negativeNumber);
+        System.out.println(reversed);
+    }
+}
