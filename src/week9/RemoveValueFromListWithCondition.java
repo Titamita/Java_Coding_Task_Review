e96795e54b596b06cea3338b4899d0a97563e988
Mixoblast <88247548+DSC9000@users.noreply.github.com>
2023-11-25 17:26:39 -0600

    Update RemoveValueFromListWithCondition.java

--- src/week9/RemoveValueFromListWithCondition.java
+++ src/week9/RemoveValueFromListWithCondition.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveValueFromListWithCondition {
+    /*
+    3) ArrayList - Remove some values 
+     Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+
+    */
     public static void main(String[] args) {
         int [] arr = {9, 5, 34, 6, 80, 78, 16, -19, 39,-18};
         System.out.println(removeValueGreaterThan(50,arr));
